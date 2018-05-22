33ff397d4b4449205932081b60b653c648d7bd2f
guguke <gbm618@gmail.com>
2018-05-22 15:47:50 +0800

    led.rbf 12M 63s

--- gpio2fpga/gpio2fpga.c
+++ gpio2fpga/gpio2fpga.c
@@ -54,7 +54,47 @@ void *gvirt_addr_clk;
 void *gvirt_data,*gvirt_dir,*gvirt_status;
 unsigned long gd1,gd0;
 unsigned long gc1,gc0;
+unsigned long v_data,v_dir,v_status,v_data0,v_data1;
+unsigned long gv_data,gv_dir,gv_status,gv_data0,gv_data1;
+unsigned long v_dataLH,v_dataHH,v_dataLL,v_dataHL;
 
+int clk_test()
+{
+  v_data = *((unsigned long *) gvirt_data);
+  v_data0 = v_data & (~0x00c7);
+  printf(" clk test loop    data:0x%08x    data0:0x%08x\n",v_data,v_data0);
+  for(;;){
+    v_data1 = v_data0 | 0x06;
+    *((unsigned long *) gvirt_data) = v_data1;
+    //mydelay(10);
+    v_data1 = v_data0 | 0x46;
+    *((unsigned long *) gvirt_data) = v_data1;
+    //mydelay(10);
+  }
+  return 0;
+}
+
+int data_test()
+{
+  v_data = *((unsigned long *) gvirt_data);
+  v_data0 = v_data & (~0x00c7);
+  printf(" data test loop    data:0x%08x    data0:0x%08x\n",v_data,v_data0);
+  for(;;){
+    v_data1 = v_data0 | 0x06;
+    *((unsigned long *) gvirt_data) = v_data1;
+    v_data1 = v_data0 | 0x46;
+    *((unsigned long *) gvirt_data) = v_data1;
+    v_data1 = v_data0 | 0x06;
+    *((unsigned long *) gvirt_data) = v_data1;
+    v_data1 = v_data0 | 0x02;
+    *((unsigned long *) gvirt_data) = v_data1;
+    v_data1 = v_data0 | 0x42;
+    *((unsigned long *) gvirt_data) = v_data1;
+    v_data1 = v_data0 | 0x02;
+    *((unsigned long *) gvirt_data) = v_data1;
+  }
+  return 0;
+}
 int mydelay(int n)
 {
     int i,s=0;
@@ -79,24 +119,20 @@ int mydelay_n1nop(int n)
     asm("nop");
     return n+1;
 }
-void output_d(int v)
+inline void output_d(int v)
 {
     if(v){
-      *((unsigned long *) gvirt_addr_data) = gd1;
-      mydelay_n1(0);
-      *((unsigned long *) gvirt_addr_clk) = gc1;
-      mydelay_n1(1);
-      mydelay_n1(2);
-      *((unsigned long *) gvirt_addr_clk) = gc0;
+      //v_data1 = v_data0 | 0x06;
+      *((unsigned long *) gvirt_data) = v_dataLH;
+      //v_data1 = v_data0 | 0x46;
+      *((unsigned long *) gvirt_data) = v_dataHH;
       return;
     }
     else{
-      *((unsigned long *) gvirt_addr_data) = gd0;
-      mydelay_n1(0);
-      *((unsigned long *) gvirt_addr_clk) = gc1;
-      mydelay_n1(1);
-      mydelay_n1(2);
-      *((unsigned long *) gvirt_addr_clk) = gc0;
+      //v_data1 = v_data0 | 0x02;
+      *((unsigned long *) gvirt_data) = v_dataLL;
+      //v_data1 = v_data0 | 0x42;
+      *((unsigned long *) gvirt_data) = v_dataHL;
       return;
     }
 }
@@ -105,21 +141,38 @@ int main(int argc, char **argv) {
     off_t target;
     off_t target_clk;
     off_t target_data,target_dir,target_status;// gpio7
-    unsigned long v_data,v_dir,v_status,v_data0,v_data1;
+    //unsigned long v_data,v_dir,v_status,v_data0,v_data1;
+    unsigned long v_status1;
     int access_type = 'w';
-    int i;
-    int ndelay=0;
+    int i,j;
     double d,d0,d1;
-    char *pbuffer;
-    int v;
+    char *pbuffer,*p;
+    int *pn;
+    int v,len;
+    FILE *fp;
+
+    printf(" usage: gpio2fpga /root/fpga/led.rbf\n");
+    if(argc<2){
+      printf(" rbf file name missing\n");
+      return 1;
+    }
+
+    fp = fopen(argv[1],"rb");
+    if(fp==NULL){
+      printf("  open file %s error\n",argv[1]);
+      return 2;
+    }
+
 	
     pbuffer = malloc(0x1400000);
     if(pbuffer==NULL){
       printf("memory not enough\n");
       return -1;
     }
-    for(i=0;i<0x1400000;i++) pbuffer[i]=0x55;
-    ndelay=atoi(argv[1]);
+
+    len=fread(pbuffer,1,0xd00000,fp);
+    printf(" file len:%d\n",len);
+    fclose(fp);
 
     target_data=0x20b4000;
     target_dir=0x20b4004;
@@ -141,45 +194,63 @@ int main(int argc, char **argv) {
     gvirt_dir = gmap_base + (target_dir & MAP_MASK);
     gvirt_status = gmap_base + (target_status & MAP_MASK);
 
-    printf(" break 0 \n");
-
     v_dir = *((unsigned long *) gvirt_dir);
-    v_dir |= 0xc7;//////////// gpio 0,1,2,6,7
+    v_dir &= ~0x00c7;//////////// gpio 0,1,2,6,7
+    v_dir |= 0x46;//////////// gpio 0,1,2,6,7
+    // gpio7.7 in....config_done
+    // gpio7.6 out...dclk
+    // gpio7.2 out....data
+    // gpio7.1 out.....nConfig
+    // gpio7.0 in.....nStatus
     *((unsigned long *) gvirt_dir) = v_dir;
-    v_data = *((unsigned long *) gvirt_data);
-    v_data1 = v_data | 0x00c7;
-    v_data0 = v_data & 0xffffff38;
-    printf(" break 01\n");
-    for(i=0;i<0x100000;i++){
-        *((unsigned long *) gvirt_data) = v_data1;
-        printf(" set 1\n");
-        v_status = *((unsigned long *) gvirt_status);
-        printf(" status: %08x\n",v_status);
-        *((unsigned long *) gvirt_data) = v_data0;
-        printf(" set 0\n");
-        v_status = *((unsigned long *) gvirt_status);
-        printf(" status: %08x\n",v_status);
-    }
-    printf(" loop set gpio7 end\n");
-    
 
-
-
-
-    read_result = *((unsigned long *) gvirt_addr_data);
-    gd1=read_result | 0x200;
-    gd0=read_result & 0xfffffdff;
-    read_result = *((unsigned long *) gvirt_addr_clk);
-    gc1=read_result | 0x200;
-    gc0=read_result & 0xfffffdff;
 
     clock_gettime(CLOCK_REALTIME,&gts0);
+    //data_test();/// loop
+    //clk_test();/// loop
 
-    for(i=0;i<0x1000000;i++){
-      *((unsigned long *) gvirt_addr_data) = gd1;
-      *((unsigned long *) gvirt_addr_data) = gd0;
-      //v = pbuffer[i] & 0x0ff;
-#if 0
+    v_data = *((unsigned long *) gvirt_data);
+    v_data0 = v_data & (~0x00c7);
+    //printf("         data0: 0x%08x\n",v_data0);
+    v_dataLH = v_data0 | 0x06;
+    v_dataHH = v_data0 | 0x46;
+    v_dataLL = v_data0 | 0x02;
+    v_dataHL = v_data0 | 0x42;
+
+    //for(i=0;;i++){
+    v_data1 = v_data0 | 0x0002;
+    *((unsigned long *) gvirt_data) = v_data1;
+    v_status = *((unsigned long *) gvirt_status);
+    for(;;){
+      v_status = *((unsigned long *) gvirt_status);
+      printf("loop0.wait.status.high    status: %08x   bit.status %d\r",v_status,v_status & 1);
+      if(1==(1&v_status))break;
+    }
+    printf("\n   nConfig.high ==> nStatus.high\n"); 
+
+    v_data1 = v_data0 | 0x00;
+    *((unsigned long *) gvirt_data) = v_data1;
+    //for(j=0;j<0x10;j++){
+    for(;;){
+      v_status = *((unsigned long *) gvirt_status);
+      printf("loop1.wait.status.low    status: %08x   bit.status %d\r",v_status,v_status & 1);
+      if(0==(1&v_status))break;
+    }
+    printf("\n      nConfig.low  ==> nStatus.low \n");
+
+    v_data1 = v_data0 | 0x0002;
+    *((unsigned long *) gvirt_data) = v_data1;
+    for(;;){
+      v_status = *((unsigned long *) gvirt_status);
+      printf("wait.status.high    status: %08x   bit.status %d\r",v_status,v_status & 1);
+      if(1==(0x1&v_status))break;
+    }
+    printf("\n  \n");
+    //}
+
+    p  = pbuffer;
+    for(i=0;i<len;i++){
+      v = *p++;
       output_d(v & 0x1);
       output_d(v & 0x2);
       output_d(v & 0x4);
@@ -188,18 +259,22 @@ int main(int argc, char **argv) {
       output_d(v & 0x20);
       output_d(v & 0x40);
       output_d(v & 0x80);
-#endif
     }
+    printf("\n wait.done \n");
+    for(i=0;;i++){
+      v_status = *((unsigned long *) gvirt_status);
+      //printf("wait.status.high    status: %08x   bit.status %d\r",v_status,v_status & 1);
+      if(0x81==(0x81&v_status))break;
+      if(0==(i&0x0fffff))printf(" %d status: 0x%08x\n",i,v_status);
+      output_d(0);
+    }
+    printf("\n   download done \n");
 
     clock_gettime(CLOCK_REALTIME,&gts1);
     d0 = gts0.tv_sec + 0.000000001 * gts0.tv_nsec;
     d1 = gts1.tv_sec + 0.000000001 * gts1.tv_nsec;
     d = d1 - d0;
     printf(" time : %.9f \n",d);
-
-    *((unsigned long *) gvirt_addr_data) = gd1;
-    *((unsigned long *) gvirt_addr_clk) = gc1;
-
 
 exit_0:
     if(munmap(gmap_base, MAP_SIZE) == -1) FATAL;
--- gpio2fpga/readme.txt
+++ gpio2fpga/readme.txt
@@ -2,4 +2,5 @@
 
 gpio2fpga:
 arm-linux-gcc -lrt gpio2fgpa.c
+arm-linux-gcc -lrt gpio2fgpa.c -O2
 
